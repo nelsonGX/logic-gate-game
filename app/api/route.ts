81309e42b1597fad463eeebd6e8deddf796225cf
nelsonGX <hi@nelsongx.com>
2025-06-06 22:36:42 +0800

    fix bugs

--- app/api/route.ts
+++ app/api/route.ts
@@ -1,5 +1,8 @@
 import { NextRequest } from 'next/server';
 
 export async function GET(request: NextRequest) {
-
+  const { searchParams } = request.nextUrl;
+  const name = searchParams.get('name') || 'World';
+  
+  return new Response(`Hello, ${name}!`);
 }
